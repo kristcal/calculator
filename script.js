e7dae298944c07d93e0c2dbb7d52f0191c1098e4
kristcal <kristinacalicc03@gmail.com>
2026-01-21 15:33:42 +0100

    add operate function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
 function add(a,b){
     return a+b;
 }
@@ -9,5 +14,21 @@ function multiply(a,b){
     return a*b;
 }
 function divide(a,b){
+    if(b===0)
+        return "Error"
     return a/b;
+}
+
+function operate(firstNumber, operator, secondNumber){
+
+
+    if(operator === "+"){
+        return add(Number(firstNumber), Number(secondNumber));
+    }else if( operator === "-"){
+        return subtract(Number(firstNumber), Number(secondNumber));
+    }else if(operator === "*"){
+        return multiply(Number(firstNumber), Number(secondNumber));
+    }else if(operator === "/"){
+        return divide(Number(firstNumber), Number(secondNumber));
+    }
 }
